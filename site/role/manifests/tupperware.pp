bbacfeef0319dc4629c2d8c79451c9c8ce1a1bee
Nicolas Corrarello <ncorrare@gmail.com>
2016-08-04 11:11:34 +0100

    Changed the webserverone container

--- site/role/manifests/tupperware.pp
+++ site/role/manifests/tupperware.pp
@@ -3,12 +3,12 @@ class role::tupperware {
     path => '/etc/hosts',
     line => '10.98.10.170 puppet',
   }
-  docker::image {'puppet/puppet-agent-centos':
+  docker::image {'centos':
 
   }
   docker::run {'webserverone':
-    image    => 'puppet-agent-centos',
-    command  => '/opt/puppetlabs/bin/puppet config set server ip-10-98-10-170.us-west-2.compute.internal --section agent; /opt/puppetlabs/bin/puppet agent -t',
+    image    => 'centos',
+    command  => 'curl -k https://ip-10-98-10-170.us-west-2.compute.internal:8140/packages/current/install.bash | sudo bash -s agent:server=ip-10-98-10-170.us-west-2.compute.internal',
     hostname => 'webserverone.container',
     ports    => ['80:80'],
   }
